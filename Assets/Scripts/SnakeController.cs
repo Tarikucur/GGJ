47b10ee0a7ef77d7db41f181f98727561aff72f2
Ahmet Çağatay <ahmetsalihcagatay@gmail.com>
2023-02-04 04:11:44 +0300

    root movement

---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+    public float SteerSpeed = 180;
+    public int Gap;
+
+    public GameObject BodyPrefab;
+
+    private List<GameObject> BodyParts = new List<GameObject>();
+    private List<Vector3> PositionHistory = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move forward
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime * -1;
+
+
+
+        // steer
+        float steerDirection = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
+
+
+        // store position history
+        PositionHistory.Insert(0, transform.position);
+
+        GrowSnake(transform);
+        
+    }
+
+    private void GrowSnake(Transform transform)
+    {
+        // GameObject body = Instantiate(BodyPrefab, transform,true);
+        var newObj = GameObject.Instantiate(BodyPrefab, transform);
+        newObj.transform.parent = GameObject.Find("Body").transform;
+    }
+}
